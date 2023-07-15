c3fa55a1bf3f93d3489f84ee40cce3620a8a5585
kelvinpraises <kelvinpraises@gmail.com>
2023-07-15 22:54:45 +0100

    feat: init main

---
+++ src/main.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import "@/index.css";
+import ErrorPage from "@/routes/error";
+
+const router = createBrowserRouter([
+  {
+    lazy: () => import("@/routes/root.layout"),
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        lazy: () => import("@/routes/index"),
+      },
+      {
+        path: "meeting/:contractAddress",
+        lazy: () => import("@/routes/meeting"),
+      },
+      {
+        path: "meeting/:contractAddress/consensus",
+        lazy: () => import("@/routes/consensus"),
+      },
+      {
+        path: "wallet/:contractAddress/",
+        lazy: () => import("@/routes/wallet"),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
