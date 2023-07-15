4eb939c568ad1015109cecc3fb00e237796b950d
kelvinpraises <kelvinpraises@gmail.com>
2023-07-15 22:51:00 +0100

    feat init component

---
+++ src/component/JoinCall.tsx
@@ -0,0 +1,7 @@
+import { VideoCallReturnType } from "@/hooks/videoCall";
+
+const JoinCall = ({ vc }: { vc: VideoCallReturnType }) => {
+  return <div>JoinCall</div>;
+};
+
+export default JoinCall;
---
+++ src/component/UploadRecord.tsx
@@ -0,0 +1,7 @@
+import { VideoCallReturnType } from "@/hooks/videoCall";
+
+const UploadRecord = ({ vc }: { vc: VideoCallReturnType }) => {
+  return <div>UploadRecord</div>;
+};
+
+export default UploadRecord;
