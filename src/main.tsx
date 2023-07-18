5e9106120d984e14ef809cb88072d2b967c31fb1
kelvinpraises <kelvinpraises@gmail.com>
2023-07-18 15:12:23 +0100

    feat: removed base

--- src/main.tsx
+++ src/main.tsx
@@ -5,33 +5,30 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "@/index.css";
 import ErrorPage from "@/routes/error";
 
-const router = createBrowserRouter(
-  [
-    {
-      lazy: () => import("@/routes/root.layout"),
-      errorElement: <ErrorPage />,
-      children: [
-        {
-          path: "/",
-          lazy: () => import("@/routes/index"),
-        },
-        {
-          path: "meeting/",
-          lazy: () => import("@/routes/meeting"),
-        },
-        {
-          path: "consensus/",
-          lazy: () => import("@/routes/consensus"),
-        },
-        {
-          path: "wallet/:contractAddress/",
-          lazy: () => import("@/routes/wallet"),
-        },
-      ],
-    },
-  ],
-  { basename: "/orionsync-coda" }
-);
+const router = createBrowserRouter([
+  {
+    lazy: () => import("@/routes/root.layout"),
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        lazy: () => import("@/routes/index"),
+      },
+      {
+        path: "meeting/",
+        lazy: () => import("@/routes/meeting"),
+      },
+      {
+        path: "consensus/",
+        lazy: () => import("@/routes/consensus"),
+      },
+      {
+        path: "wallet/:contractAddress/",
+        lazy: () => import("@/routes/wallet"),
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
