0b5a2a8115941de7931c053e26d548c45b4ecc2d
kelvinpraises <kelvinpraises@gmail.com>
2023-07-18 15:18:28 +0100

    feat: hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 
 import "@/index.css";
 import ErrorPage from "@/routes/error";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     lazy: () => import("@/routes/root.layout"),
     errorElement: <ErrorPage />,
