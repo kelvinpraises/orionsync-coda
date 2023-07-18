4b4f8f0ae26b447945966acb0a09f98060af95a8
kelvinpraises <kelvinpraises@gmail.com>
2023-07-18 14:18:43 +0100

    Adding dist

--- src/main.tsx
+++ src/main.tsx
@@ -5,30 +5,33 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "@/index.css";
 import ErrorPage from "@/routes/error";
 
-const router = createBrowserRouter([
-  {
-    lazy: () => import("@/routes/root.layout"),
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        lazy: () => import("@/routes/index"),
-      },
-      {
-        path: "meeting/",
-        lazy: () => import("@/routes/meeting"),
-      },
-      {
-        path: "consensus/",
-        lazy: () => import("@/routes/consensus"),
-      },
-      {
-        path: "wallet/:contractAddress/",
-        lazy: () => import("@/routes/wallet"),
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      lazy: () => import("@/routes/root.layout"),
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          lazy: () => import("@/routes/index"),
+        },
+        {
+          path: "meeting/",
+          lazy: () => import("@/routes/meeting"),
+        },
+        {
+          path: "consensus/",
+          lazy: () => import("@/routes/consensus"),
+        },
+        {
+          path: "wallet/:contractAddress/",
+          lazy: () => import("@/routes/wallet"),
+        },
+      ],
+    },
+  ],
+  { basename: "/orionsync-coda" }
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <RouterProvider router={router} />
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/orionsync-coda/',
   plugins: [react()],
   resolve: {
     alias: [
